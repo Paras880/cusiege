660ae87157aaaf4591d38c8521dca0272c9e28a6
Paras880 <56482932+Paras880@users.noreply.github.com>
2019-10-12 21:32:44 +0530

    Update input.php

--- input.php
+++ input.php
@@ -2,8 +2,10 @@
 <html>
 <head>
  <link rel='icon' href='icon/favicon.ico' type='image/x-icon'>
-<meta charset="utf-8">
+
 <title>Untitled Document</title>
+ <title>Untitled Document</title>
+ <title>Untitled Document</title>
   <link rel='icon' href='icon/favicon.ico' type='image/x-icon'>
 </head>
 
